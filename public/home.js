81c3d1365e47fde2a222cdf853be2e75113e469b
Felipe Vieira <92130047+FelipeMielkeVieira@users.noreply.github.com>
2022-08-07 14:47:11 -0300

    07/08 - 14:47

---
+++ public/home.js
@@ -0,0 +1,69 @@
+let totalWidth = window.innerWidth * 0.98;
+let totalHeight = window.innerHeight * 0.96;
+
+let speed = 200;
+
+kaboom({
+    debug: true,
+    font: "sinko",
+    width: totalWidth,
+    height: totalHeight,
+    background: [255, 255, 255]
+});
+
+let bgImage = loadSprite("capa", "/img/capa.jpg");
+
+scene("inicio", () => {
+
+    add([
+        sprite("capa"),
+        pos(width() / 2, height() / 2),
+        origin("center"),
+        scale(1),
+        fixed(),
+    ]);
+
+    add([
+        text("Terras do Sem-Fim"),
+        pos(totalWidth * 0.3, totalHeight * 0.2),
+        scale(4),
+        color(255, 128, 0)
+    ])
+
+    add([
+        text("Jorge Amado"),
+        pos(totalWidth * 0.45, totalHeight * 0.3),
+        scale(2),
+    ])
+
+    add([
+        text("Feito por Felipe Vieira e Kenzo Sato"),
+        pos(totalWidth * 0.01, totalHeight * 0.95),
+        scale(1.5),
+    ])
+
+    add([
+        pos(totalWidth * 0.425, totalHeight * 0.65),
+        color(255, 128, 0),
+        rect(totalWidth * 0.15, 40),
+        outline(2)
+    ])
+
+    add([
+        "botaoContinuar",
+        text("Continuar"),
+        pos(totalWidth * 0.45, totalHeight * 0.67),
+        scale(2),
+        area()
+    ])
+
+    onClick("botaoContinuar", () => {
+        go("parte1");
+    })
+})
+
+scene("parte1", () => {
+
+})
+
+go("inicio");
