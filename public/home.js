68eb9545034f3aad9b3c99f6d8761b2f04f88402
larissa71 <felipe_mielke-vieira@estudante.sc.senai.br>
2022-08-09 09:40:29 -0300

    09/08 - 09:40

--- public/home.js
+++ public/home.js
@@ -16,6 +16,12 @@ let bgImage = loadSprite("capa", "/img/capa.jpg");
 loadSprite("grama", "https://i.ibb.co/XzwXz89/New-Piskel-5.png");
 loadSprite("porto", "/sprites/porto.png");
 loadSprite("arvore", "/sprites/arvore.png")
+loadSprite("player", "/sprites/player.png");
+
+loadSprite("antonio", "/sprites/antonio.png");
+loadSprite("joao", "/sprites/joao.png");
+loadSprite("margot", "/sprites/margot.png");
+loadSprite("virgilio", "/sprites/virgilio.png");
 
 scene("inicio", () => {
 
@@ -74,13 +80,20 @@ scene("parte1", () => {
         "3"
     ], "2")
 
-    
+    add([
+        "agua",
+        rect(totalWidth * 0.1, totalHeight * 0.1),
+        pos(0, totalHeight * 0.9),
+        color(0, 0, 255)
+    ])
 
     add([
         "porto",
         sprite("porto"),
         scale(8),
-        pos(totalWidth * 0.1, totalHeight * 0.9)
+        pos(totalWidth * 0.1, totalHeight * 0.9),
+        area(),
+        solid()
     ])
 
     //Grama 1
@@ -88,7 +101,9 @@ scene("parte1", () => {
         "grama",
         sprite("grama"),
         scale(6),
-        pos(totalWidth * 0.4, totalHeight * 0.9)
+        pos(totalWidth * 0.4, totalHeight * 0.9),
+        area(),
+        solid()
     ])
 
     //Grama 2
@@ -96,7 +111,9 @@ scene("parte1", () => {
         "grama",
         sprite("grama"),
         scale(6),
-        pos(totalWidth * 0.5, totalHeight * 0.9)
+        pos(totalWidth * 0.5, totalHeight * 0.9),
+        area(),
+        solid()
     ])
 
     //Grama 3
@@ -104,7 +121,9 @@ scene("parte1", () => {
         "grama",
         sprite("grama"),
         scale(6),
-        pos(totalWidth * 0.6, totalHeight * 0.9)
+        pos(totalWidth * 0.6, totalHeight * 0.9),
+        area(),
+        solid()
     ])
 
     //Grama 4
@@ -112,7 +131,9 @@ scene("parte1", () => {
         "grama",
         sprite("grama"),
         scale(6),
-        pos(totalWidth * 0.7, totalHeight * 0.9)
+        pos(totalWidth * 0.7, totalHeight * 0.9),
+        area(),
+        solid()
     ])
 
     //Grama 5
@@ -120,7 +141,9 @@ scene("parte1", () => {
         "grama",
         sprite("grama"),
         scale(6),
-        pos(totalWidth * 0.8, totalHeight * 0.9)
+        pos(totalWidth * 0.8, totalHeight * 0.9),
+        area(),
+        solid()
     ])
 
     //Grama 6
@@ -128,24 +151,123 @@ scene("parte1", () => {
         "grama",
         sprite("grama"),
         scale(6),
-        pos(totalWidth * 0.9, totalHeight * 0.9)
+        pos(totalWidth * 0.9, totalHeight * 0.9),
+        area(),
+        solid()
     ])
 
     //Árvore 1
     add([
         "arvore",
         sprite("arvore"),
-        scale(6),
-        pos(totalWidth * 0.6, totalHeight * 0.6)
+        scale(8),
+        pos(totalWidth * 0.6, totalHeight * 0.5),
     ])
 
     //Árvore 2
     add([
         "arvore",
         sprite("arvore"),
-        scale(6),
-        pos(totalWidth * 0.75, totalHeight * 0.65)
+        scale(8),
+        pos(totalWidth * 0.75, totalHeight * 0.55)
     ])
+
+    const player = add([
+        "player",
+        sprite("player"),
+        pos(totalWidth * 0.1, totalHeight * 0.7),
+        area(),
+        solid(),
+        scale(3),
+        body()
+    ])
+
+    onKeyDown("left", () => {
+        player.move(-speed, 0);
+    })
+    onKeyDown("a", () => {
+        player.move(-speed, 0);
+    })
+
+    onKeyDown("right", () => {
+        player.move(speed, 0);
+    })
+    onKeyDown("d", () => {
+        player.move(speed, 0);
+    })
+
+    onKeyDown("up", () => {
+        player.jump(100)
+    })
+    onKeyDown("w", () => {
+        player.jump(100)
+    })
+
+    onKeyDown("down", () => {
+        player.move(0, speed);
+    })
+    onKeyDown("s", () => {
+        player.move(0, speed);
+    })
+
+    const antonio = add([
+        "antonio",
+        sprite("antonio"),
+        pos(totalWidth * 0.2, totalHeight * 0.7),
+        area(),
+        scale(4)
+    ])
+
+    const joao = add([
+        "joao",
+        sprite("joao"),
+        pos(totalWidth * 0.3, totalHeight * 0.7),
+        area(),
+        scale(4)
+    ])
+
+    const margot = add([
+        "margot",
+        sprite("margot"),
+        pos(totalWidth * 0.4, totalHeight * 0.7),
+        area(),
+        scale(4)
+    ])
+
+    const virgilio = add([
+        "virgilio",
+        sprite("virgilio"),
+        pos(totalWidth * 0.9, totalHeight * 0.7),
+        area(),
+        scale(4)
+    ])
+
+    let textAntonio = false;
+    player.onCollide("antonio", () => {
+        if(!textAntonio) {
+            textAntonio = true;
+            add([
+               "text",
+               text("Queria tanto uma roça de cacau minha!"),
+               pos(antonio.pos.width, antonio.pos.height + (totalHeight * 0.2)),
+               scale(2)
+            ])
+            setTimeout(() => {
+                textAntonio = false;
+            }, 4000);
+        }
+    })
+
+    onUpdate(() => {
+        if(player.pos.x >= totalWidth) {
+            go("parte2")
+        }
+    })
+
+})
+
+scene("parte2", () => {
+
 })
 
 go("inicio");
