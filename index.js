f13dfc32ab434bff8ae304f177033e45b8517e0a
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-08-15 20:44:33 -0300

    15/08 - 20:44

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 
-const path = require("path");
-
 app.use(express.static("public"))
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/home.html"));
+  res.send("<h1>Hello World!</h1>")
 })
   
 app.listen(process.env.PORT || 3000, () => {
